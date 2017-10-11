08bf26ae71ccf0f50dc67d5395996154a9240580
Eddie Atkinson <eddiebatkinson@gmail.com>
2017-10-11 15:05:22 -0400

    ticker '

--- scripts.js
+++ scripts.js
@@ -7,7 +7,21 @@ $(document).ready(()=>{
 	$('.yahoo-finance-form').submit((event)=>{
 		// Stop the browser from sending the page on...we will handle it
 		event.preventDefault();
-		console.log("User submitted the form!");
+		var stockSymbol = $('#ticker').val();
+		var url = `http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22${stockSymbol}%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json`;
+		$.getJSON(url, (theDataJSFound)=>{
+			console.log(theDataJSFound);
+			var stockInfo = theDataJSFound.query.results.quote;
+			var newRow = '';
+			newRow += `<tr>`;
+				newRow += `<td>${stockInfo.Symbol}</td>`;
+				newRow += `<td>${stockInfo.Name}</td>`;
+				newRow += `<td>${stockInfo.Ask}</td>`;
+				newRow += `<td>${stockInfo.Bid}</td>`;
+				newRow += `<td>${stockInfo.Change}</td>`;
+			newRow += `</tr>`;
+			$('#stock-table-body').html(newRow);
+		})
 	})
-	var url = 'http://query.yahooapis.com/v1/public/yql?q=env%20%27store://datatables.org/alltableswithkeys%27;select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22goog%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json';
+	
 })
